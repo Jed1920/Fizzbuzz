d9cd2560d6ed9a162456be2a3f905018f30a97e1
Jed Johnson <jed.johnson@softwire.com>
2019-10-21 16:08:48 +0100

    FizzBuzzBang, Add 7
    Bang if multiple 7
    FizzBang if 3 and 7
    BuzzBang if 5 and 7
    FizzBuzzBang if 3,5,7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,23 @@ public class Main {
 
         for (int x = 1; x <= 100; x++) {
             if (x % 3 == 0) {
-                if (x % 5 == 0) {
-                    System.out.println("Fizzbuzz");
+                if (x % 5 == 0 && x % 7 == 0) {
+                    System.out.println("FizzBuzzBang");
+                } else if (x % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (x % 7 == 0) {
+                    System.out.println("FizzBang");
                 } else {
                     System.out.println("Fizz");
                 }
             } else if (x % 5 == 0) {
-                System.out.println("Buzz");
+                if (x % 7 == 0) {
+                    System.out.println("BuzzBang");
+                } else {
+                    System.out.println("Buzz");
+                }
+            } else if (x % 7 == 0) {
+                        System.out.println("Bang") ;
             } else {
                 System.out.println(x);
             }
