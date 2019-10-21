eab33cf2243c5b7d127c42b18056903a4c662612
Jed Johnson <jed.johnson@softwire.com>
2019-10-21 17:21:24 +0100

    Finish 21/10/19

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,25 @@ public class Main {
 
         for (int x = 1; x <= 106; x++) {
             String a = "";
-
-            if (x % 11 == 0) {
-                a += "Bong";
+            if (x % 3 == 0) {
+                a += "Fizz";
             }
-            else {
-                if (x % 3 == 0) {
-                    a += "Fizz";
-                }
-                if (x % 5 == 0) {
-                    a += "Buzz";
-                }
-                if (x % 7 == 0) {
-                    a += "Bang";
-                }
-                if (a.isEmpty()) {
-                    a += x;
-                }
+            if (x % 5 == 0) {
+                a += "Buzz";
+            }
+            if (x % 7 == 0) {
+                a += "Bang";
+            }
+            if (x % 11 == 0) {
+                a = "Bong";
+            }
+            if (x % 13 == 0) {
+                String b = "";
+                b = "Fezz";
+
+            }
+            if (a.isEmpty()) {
+                a += x;
             }
             System.out.println(a);
         }
