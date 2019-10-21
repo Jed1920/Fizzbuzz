1db53c435fb50cd6fb498481a907dcbf1084bc05
Jed Johnson <jed.johnson@softwire.com>
2019-10-21 16:42:08 +0100

    Bong, Add 11
    Only Bong at multiples of 11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int x = 1; x <= 100; x++) {
+        for (int x = 1; x <= 106; x++) {
             String a = "";
-            if (x % 3 == 0) {
-                a += "Fizz";
+
+            if (x % 11 == 0) {
+                a += "Bong";
             }
-            if (x % 5 == 0) {
-                a += "Buzz";
-            }
-            if (x % 7 == 0) {
-                a += "Bang";
-            }
-            if (a.isEmpty()) {
-                a += x;
+            else {
+                if (x % 3 == 0) {
+                    a += "Fizz";
+                }
+                if (x % 5 == 0) {
+                    a += "Buzz";
+                }
+                if (x % 7 == 0) {
+                    a += "Bang";
+                }
+                if (a.isEmpty()) {
+                    a += x;
+                }
             }
             System.out.println(a);
         }
     }
 }
-
